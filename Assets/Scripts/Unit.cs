41a5765682bae72da79c909ffb80d2bd4ffba100
serg11111 <vardanarsb@gmail.com>
2019-11-21 09:37:53 +0200

    Add UnitScripts
    Add UnitScripts

---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    public virtual void ReceiveDamage()
+    {
+        Die();
+    }
+
+    public virtual void Die()
+    {
+        Destroy(gameObject);
+    }
+
+}
