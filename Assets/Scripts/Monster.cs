0247f2a4edbb65151b56c27e2169a041f0504e36
serg11111 <vardanarsb@gmail.com>
2019-11-22 22:49:19 +0200

    Улучшил мкрипт монстра-червяка
    Улучшил мкрипт монстра-червяка

--- Assets/Scripts/Monster.cs
+++ Assets/Scripts/Monster.cs
@@ -2,17 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Monster : MonoBehaviour
+public class Monster : Unit
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter2D(Collider2D collider)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Bullet bullet = collider.GetComponent<Bullet>();
+        if (bullet)
+        {
+            ReceiveDamage();
+        }
     }
 }
